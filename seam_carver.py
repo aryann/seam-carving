63d78c37c60f36568a27a4ee9cec1f281c274b9d
Aryan Naraghi <aryan.naraghi@gmail.com>
2019-07-05 08:46:38 -0700

    Add function to compute seam costs

--- seam_carver.py
+++ seam_carver.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 import PIL
@@ -7,7 +8,7 @@ from matplotlib import image
 from matplotlib import pyplot
 
 
-_RESIZE_FACTOR = 1
+_RESIZE_FACTOR = .2
 
 # Determines while energy levels are visualized. Any cell value that
 # is below this percentile is displayed as black. Cell values at or
@@ -17,7 +18,7 @@ _ENERGY_PERCENTILE = 95
 
 def compute_energy(data):
     """N.B.: energies for the edges are not computed."""
-    result = np.zeros(data.shape, dtype=np.int32)
+    result = np.zeros(data.shape[:2], dtype=np.int32)
 
     for i in range(1, data.shape[0] - 1):
         for j in range(1, data.shape[1] - 1):
@@ -36,20 +37,54 @@ def compute_energy(data):
     return result[1:-1, 1:-1]
 
 
+def compute_seam_costs(energy):
+    """Computes the seam costs for the given image energy matrix. Seams
+    are computed from the top row to the bottom row. This function
+    returns two matrices:
+
+      (1) A matrix that contains seam costs for all cells. For each
+          cell, the cost is the energy at that cell + the minimum cost
+          of the closest three cells in the row above the cell.
+
+      (2) A matrix that contains the indices required to traverse each
+          seam. For each cell, the value contains the index of the
+          cell in the above row that led to the minimum seam that
+          terminates at that cell.
+
+    """
+    costs = np.full((energy.shape[0], energy.shape[1] + 2),
+                    fill_value=np.iinfo(np.int64).max,
+                    dtype=np.int64)
+    indices = np.zeros(costs.shape, dtype=np.int32)
+
+    for j in range(1, costs.shape[1] - 1):
+        costs[0, j] = energy[0, j - 1]
+        indices[0, j] = -1
+
+    for i in range(1, costs.shape[0]):
+        for j in range(1, costs.shape[1] - 1):
+            items = costs[i - 1, j - 1 : j + 2]
+            costs[i, j] = energy[i, j - 1] + items.min()
+            indices[i, j] = j + items.argmin() - 2
+
+    return costs[:, 1:-1], indices[:, 1:-1]
+
+
 if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
     image_obj = PIL.Image.open(sys.argv[1])
     image_obj.thumbnail((image_obj.size[0] * _RESIZE_FACTOR,
                          image_obj.size[1] * _RESIZE_FACTOR))
 
     data = np.array(image_obj)
-    print(data.dtype)
-    print(data.shape)
+    logging.info('Image shape: %s', data.shape)
 
     np.pad(data, pad_width=1, mode='edge')
 
     energy = compute_energy(data)
+    logging.info('Finished computing image energy: %s', energy)
+    logging.info('Finished computing seam costs: %s', compute_seam_costs(energy))
     energy[energy < np.percentile(energy, _ENERGY_PERCENTILE)] = 0
-    print(energy)
 
     figure = pyplot.figure()
 
